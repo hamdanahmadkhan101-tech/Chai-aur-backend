4daacb3a8176e94738f81d3cc981bf61781d6c57
hamdanahmadkhan101-tech <hamdanahmadkhan101@gmail.com>
2026-01-03 12:28:21 +0500

    feat: move error middleware import and usage to appropriate location

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import errorMiddleware from './middlewares/errorMiddleware.js';
 
 const app = express();
 
@@ -14,5 +15,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
+
+// Routes will go here
+// app.use('/api/v1/users', userRoutes);
+
+// Error middleware must be LAST
+app.use(errorMiddleware);
 
 export default app;
